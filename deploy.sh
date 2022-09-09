f44b9fee07c55ff2cda8217b04bd6aab8719ee36
Floy <floy-dev@mail.ru>
2022-09-09 13:36:14 +0300

    fast commit

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,20 @@ WARNING='\033[1;33m'
 ERROR='\033[0;31m'
 NC='\033[0m'
 
-WEBSITE_ABSOLUTE_PATH=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../website")
-DASHBOARD_ABSOLUTE_PATH=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../dashboard")
+ENV_ABSOLUTE_PATH=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.env.local")
+if test -f "$ENV_ABSOLUTE_PATH"; then
+    CONTAINER_NAME_RAW=$(cat "$ENV_ABSOLUTE_PATH" | grep "CONTAINER_NAME")
+    IFS='=' read -r -a CONTAINER_NAME_ARRAY <<< "$CONTAINER_NAME_RAW"
+    CONTAINER_NAME="${CONTAINER_NAME_ARRAY[1]}"
+    echo ""
+    printf "%bdeploying %s%b\n" "$SUCCESS" "$CONTAINER_NAME" "$NC"
+    echo ""
+else
+    echo ""
+    printf "%b%s do not exist%b\n" "$WARNING" "$ENV_ABSOLUTE_PATH" "$NC"
+    echo ""
+    exit
+fi
 
 DEPLOY_COMPOSER=false
 DEPLOY_YARN_DASHBOARD=false
@@ -49,30 +61,20 @@ while getopts "dwcph" option; do
     esac
 done
 
-if [ -d "$DASHBOARD_ABSOLUTE_PATH" ]; then
-    if [ $DEPLOY_YARN_DASHBOARD == true ]; then
-        yarn --cwd "$DASHBOARD_ABSOLUTE_PATH"
-        yarn encore prod --cwd "$DASHBOARD_ABSOLUTE_PATH"
-    fi
-else
-    echo ""
-    printf "%b%s do not exist%b\n" "$WARNING" "$DASHBOARD_ABSOLUTE_PATH" "$NC"
-    echo ""
+if [ $DEPLOY_YARN_DASHBOARD == true ]; then
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec yarn --cwd /var/www/app/dashboard"
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec yarn --cwd /var/www/app/dashboard encore prod"
 fi
 
-if [ -d "$WEBSITE_ABSOLUTE_PATH" ]; then
-    if [ $DEPLOY_COMPOSER == true ]; then
-        composer i -d "$WEBSITE_ABSOLUTE_PATH"
-    fi
-    if [ $DEPLOY_YARN_WEBSITE == true ]; then
-        yarn --cwd "$WEBSITE_ABSOLUTE_PATH"
-        yarn encore prod --cwd "$WEBSITE_ABSOLUTE_PATH"
-    fi
-    if [ $UPDATE_CACHE == true ]; then
-        php "$WEBSITE_ABSOLUTE_PATH/bin/console" c:c
-    fi
-else
-    echo ""
-    printf "%b%s do not exist%b\n" "$WARNING" "$WEBSITE_ABSOLUTE_PATH" "$NC"
-    echo ""
+if [ $DEPLOY_YARN_WEBSITE == true ]; then
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec yarn --cwd /var/www/app/website"
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec yarn --cwd /var/www/app/website encore prod"
+fi
+
+if [ $DEPLOY_COMPOSER == true ]; then
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec composer i -d /var/www/app/website"
+fi
+
+if [ $UPDATE_CACHE == true ]; then
+    docker exec -it "$CONTAINER_NAME-php" bash -c "exec php /var/www/app/website/bin/console c:c"
 fi
